87d2b5bf35c85beb14548d603d1a0a9d62f017e7
Brijraj Kacha <89012606+BR-Kacha@users.noreply.github.com>
2024-06-25 12:52:51 +0530

    Update GH_datalogger.ino

--- GH_datalogger/GH_datalogger.ino
+++ GH_datalogger/GH_datalogger.ino
@@ -1,36 +1,3 @@
-/*
---------------LCD Part Info---------------------------
-
- The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 6
- * LCD D7 pin to digital pin 7
- * LCD R/W pin to ground
- * LCD VSS pin to ground
- * LCD VCC pin to 5V
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
-
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- modified 7 Nov 2016
- by Arturo Guadalupi
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystalHelloWorld
-
-*/
 
 #include <LiquidCrystal.h>
 #include <SoftwareSerial.h>
@@ -43,9 +10,9 @@ const int CURRENT_SENSOR_PIN = A0;  //for current
 
 float voltage = 0.0;
 const float MAX_CURRENT = 5.0; // Amps
-String AP = "Railwire";       // AP NAME
-String PASS = "VU2AXQ.."; // AP PASSWORD
-String API = "REXNGNOABE2RVB7Y";   // Write API KEY
+String AP = "";       // AP NAME
+String PASS = ""; // AP PASSWORD
+String API = "";   // Write API KEY
 String HOST = "api.thingspeak.com";
 String PORT = "80";
 String field = "field1";
